6b3d244d2e01ca87bf8f393a996eee49a8e91ad7
Andrew Dieffenbach <puzzud@gmail.com>
2017-10-27 09:46:43 -0400

    Switched Mover.js to use "legacy scripting system".

--- Mover.js
+++ Mover.js
@@ -1,4 +1,5 @@
 // Mover.js
+/*
 var Mover = pc.createScript('mover');
 
 Mover.prototype.initialize = function()
@@ -12,3 +13,28 @@ Mover.prototype.update = function(dt)
   position.x += dt * 0.15;
   this.entity.setPosition(position);
 };
+*/
+
+function Mover(app)
+{
+	var ScriptObject = function(entity)
+	{
+			this.entity = entity;
+	};
+
+	return ScriptObject;
+};
+
+Mover.prototype.initialize = function()
+{
+  
+};
+
+Mover.prototype.update = function(dt)
+{
+	var position = this.entity.getPosition();
+	position.x += dt * 0.15;
+	this.entity.setPosition(position);
+};
+
+pc.script.create("mover", Mover);
