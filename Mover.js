69393899f4cdf323a7753257a7114e0594ee6700
Andrew Dieffenbach <puzzud@gmail.com>
2017-10-27 09:40:27 -0400

    Added test movement logic.

--- Mover.js
+++ Mover.js
@@ -1,1 +1,14 @@
 // Mover.js
+var Mover = pc.createScript('mover');
+
+Mover.prototype.initialize = function()
+{
+  
+};
+
+Mover.prototype.update = function(dt)
+{
+  var position = this.entity.getPosition();
+  position.x += dt * 0.15;
+  this.entity.setPosition(position);
+};
