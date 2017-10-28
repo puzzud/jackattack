183c273da24e09bac0dfcd3dce30e86bf0c2c845
puzzud <puzzud@gmail.com>
2017-10-28 15:46:20 -0400

    Add attribute to test script?

--- Mover.js
+++ Mover.js
@@ -1,49 +1,11 @@
-// Mover.js
-/*
-var Mover = pc.createScript('mover');
-
-Mover.prototype.initialize = function()
-{
-  
-};
-
-Mover.prototype.update = function(dt)
-{
-  var position = this.entity.getPosition();
-  position.x += dt * 0.15;
-  this.entity.setPosition(position);
-};
-*/
-/*
-function Mover(app)
-{
-	var ScriptObject = function(entity)
-	{
-			this.entity = entity;
-	};
-
-	return ScriptObject;
-};
-
-Mover.prototype.initialize = function()
-{
-  
-};
-
-Mover.prototype.update = function(dt)
-{
-	var position = this.entity.getPosition();
-	position.x += dt * 0.15;
-	this.entity.setPosition(position);
-};
-
-pc.script.create("mover", Mover);
-*/
+pc.script.attribute('speed', 'number', 1.5);
 
 var Mover = function (entity)
 {
 	this.entity = entity;
 };
+
+pc.script.create("mover", function(app){return Mover;});
 
 Mover.prototype.initialize = function()
 {
@@ -53,8 +15,7 @@ Mover.prototype.initialize = function()
 Mover.prototype.update = function(dt)
 {
 	var position = this.entity.getPosition();
-	position.x += dt * 0.15;
+	position.x += dt * this.speed;
 	this.entity.setPosition(position);
 };
 
-pc.script.create("mover", function(app){return Mover;});
